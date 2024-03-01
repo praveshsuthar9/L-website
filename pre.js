14677a96d46650de469263bb93ceda70f2df04d2
praveshsuthar9 <praveshsuthar9@gmail.com>
2024-03-01 20:47:05 +0530

    First commit

---
+++ pre.js
@@ -0,0 +1,52 @@
+
+// // let number  = [25,36,49,64,81];
+
+
+// // let newnum = number.map((e) => {
+// //     return(
+// //         Math.sqrt(e)
+// //     )
+// // })
+
+// // console.log(newnum)
+
+// // let arr = [2,3,4,6,8];
+
+// // let arr2 = arr.map((e) => {
+// //     return e * 2;
+    
+// // }).filter((e) => {
+// //     return e > 10
+// // }).reduce((a,e) => {
+// //     return a+= e
+// // },7)
+
+
+// // console.log(arr2)
+
+// // let arr = [5,5,1]
+
+
+// // console.log(arr)
+// // let arr2 = arr.reduce((a,e) => {
+// //     return a += e
+// // },7) 
+
+// // console.log(arr2)
+
+// let arr = [
+//     ['arr1','arr2'],
+//     ['arr3','arr4'],
+//     ['arr5','arr6'],
+//     ['arr7','arr8']
+// ]
+
+// console.log(arr)
+
+// let arr2 = arr.reduce((a,e) => {
+//     return a.concat(e)
+// })
+
+// console.log(arr2)
+
+let myName = "Prvaesh Suthar Bankora";
